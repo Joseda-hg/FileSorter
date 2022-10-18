946389a4ca4c32a178d44b9bc61331cb16cedabd
joseda <josedhg29@gmail.com>
2022-10-18 00:20:22 -0300

    eh

---
+++ sorter.js
@@ -0,0 +1,12 @@
+let directorio = ""
+
+// Get files from directorio
+
+// Definir clase de reglas y reglas para el movimiento
+
+
+for (i in directorio){
+    console.log(i)
+    // Mover elementos individuales segun su regla
+}
+
